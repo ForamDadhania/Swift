3378a1bb7c198f2064084f7184b3c4caafa66ff9
ForamDadhania <39779290+ForamDadhania@users.noreply.github.com>
2018-07-28 15:59:46 -0400

    Add files via upload

---
+++ Foram/AirlineReservation/AirlineReservation/User.swift
@@ -0,0 +1,64 @@
+//
+//  User.swift
+//  AirlineReservation
+//
+//  Created by MacStudent on 2018-07-28.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Login{
+    private var userName : String?
+    private var password : String?
+    
+    init(){
+        self.userName = ""
+        self.password = ""
+    }
+    
+    init(userName : "Foram", password : "123"){
+        self.userName = "Foram"
+        self.password = "123"
+    }
+    
+    var Username : String?{
+        get{ return self.userName }
+        set{ self.userName = newValue }
+    }
+    
+    var Password : String?{
+        get{ return self.password }
+        set{ self.password = newValue }
+    }
+    
+    func displayData() -> String{
+        var returnData = ""
+        
+        if self.userName != nil{
+            returnData += " Username : " + self.userName!
+        }
+        
+        if self.password != nil{
+            returnData += " Password : " + self.password!
+        }
+        return returnData
+        
+    }
+    func userData(){
+        print("Username : ")
+        self.userName = readLine()!
+        if userName != self.userName {
+            print("Please Enter a valid Username")
+        }else {
+            print("Bravo")
+        }
+        print("Password : ")
+        self.password = readLine()!
+        if password != self.password{
+            print("Please Enter a valid Password")
+        }else {
+            print("Bravo")
+        }
+    }
+    
+}
---
+++ Foram/AirlineReservation/AirlineReservation/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  AirlineReservation
+//
+//  Created by MacStudent on 2018-07-28.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+//var foram = Flight()
+//print(foram.display())
+//print(foram.displayData())
+
+//var seat = Seats()
+//print(seat.seatnumber())
+
+var matrix = Matrix(rows: 3, columns: 4)
+print(matrix[0,0])
+
