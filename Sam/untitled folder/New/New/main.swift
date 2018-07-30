a21f73b9386e9b695510714c1bfaeea3bd5146e4
ForamDadhania <39779290+ForamDadhania@users.noreply.github.com>
2018-07-30 17:56:01 -0400

    Add files via upload

---
+++ Sam/untitled folder/New/New/main.swift
@@ -0,0 +1,49 @@
+//
+//  main.swift
+//  New
+//
+//  Created by MacStudent on 2018-07-28.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+//var foram = Customer()
+//print(foram.displayData())
+//print(foram.registerUser())
+
+var choice = 1
+let dataHelper = DataHelper()
+var order = Order()
+
+while choice != 6{
+    print("\n----What would you like to do today !----")
+    print("\t 1 : Browse Products ")
+    print("\t 2 : Add Order ")
+    print("\t 3 : Show Order ")
+    print("\t 4 : Update Order ")
+    print("\t 5 : Cancel Order ")
+    print("\t 6 : Exit ")
+    print("-----------------------------------------")
+    print("Enter you choice please : ")
+    choice = (Int)(readLine()!)!
+    
+    switch choice{
+    case 1:
+        dataHelper.displayProducts()
+    case 2:
+        order.addOrder()
+    case 3:
+        print(order.displayData())
+        //    case 4:
+    //        order.updateOrder()
+    case 5:
+        order.cancelOrder()
+    case 6:
+        exit(0)
+    default:
+        print("Please enter valid menu option.")
+    }
+}
+
+
+
